d00e9c8865ae8194523aac59e9d8fdc7843b0d01
peterkhayes <peter.k.hayes@gmail.com>
2015-02-06 11:36:47 -0800

    use array instead of fake redis for in-memory storage

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var assert = require("assert");
 var microtime = require("microtime-nodejs");
-var fakeredis = require("fakeredis");
-fakeredis.fast = true; // Remove latency.
 
 function RateLimiter (options) {
   var redis           = options.redis,
       interval        = options.interval,
       maxInInterval   = options.maxInInterval,
       minDifference   = options.minDifference,
-      namespace       = options.namespace || (options.redis ? ("rate-limiter-" + Math.random().toString(36).slice(2)) : "");
-
-  redis = redis || fakeredis.createClient();
+      namespace       = options.namespace || (options.redis && ("rate-limiter-" + Math.random().toString(36).slice(2))) || null;
 
   assert(interval > 0, "Must pass a positive integer for `options.interval`");
   assert(maxInInterval > 0, "Must pass a positive integer for `options.maxInInterval`");
@@ -20,35 +16,74 @@ function RateLimiter (options) {
   interval *= 1000;
   minDifference *= 1000;
 
-  return function (id, cb) {
-    if (!cb) {
-      cb = id;
-      id = "";
-    }
-    
-    assert.equal(typeof cb, "function", "Callback must be a function.");
-    
-    var now = microtime.now();
-    var key = namespace + id;
-    var clearBefore = now - interval;
+  if (!options.redis) {
+    var storage = {};
+    var timeouts = {};
+  }
 
-    var batch = redis.multi();
-    batch.zremrangebyscore(key, 0, clearBefore);
-    batch.zrange(key, 0, -1);
-    batch.zadd(key, now, now);
-    batch.exec(function (err, resultArr) {
-      if (err) return cb(err);
-  
-      var oldMembers = resultArr[1].map(Number);
-      var tooManyActionsInInterval = oldMembers.length >= maxInInterval;
-      var previousActionTooRecent = minDifference && (now - oldMembers.pop() < minDifference);
+  if (redis) {
+    return function (id, cb) {
+      if (!cb) {
+        cb = id;
+        id = "";
+      }
+      
+      assert.equal(typeof cb, "function", "Callback must be a function.");
+      
+      var now = microtime.now();
+      var key = namespace + id;
+      var clearBefore = now - interval;
+
+      var batch = redis.multi();
+      batch.expire(key, interval);
+      batch.zremrangebyscore(key, 0, clearBefore);
+      batch.zrange(key, 0, -1);
+      batch.zadd(key, now, now);
+      batch.exec(function (err, resultArr) {
+        if (err) return cb(err);
+    
+        var oldMembers = resultArr[2].map(Number);
+        var tooManyActionsInInterval = oldMembers.length >= maxInInterval;
+        var previousActionTooRecent = minDifference && (now - oldMembers.pop() < minDifference);
+
+        return cb(null, !tooManyActionsInInterval && !previousActionTooRecent);
+
+      });
+    }
+  } else {
+    return function (id, cb) {
+      if (!cb) {
+        cb = id;
+        id = "";
+      }
+      
+      assert.equal(typeof cb, "function", "Callback must be a function.");
+      
+      var now = microtime.now();
+      var key = namespace + id;
+      var clearBefore = now - interval;
+
+      clearTimeout(timeouts[id]);
+      var userStorage = storage[id] = (storage[id] || []).filter(function(timestamp) {
+        return timestamp > clearBefore;
+      });
+      
+      var tooManyActionsInInterval = userStorage.length >= maxInInterval;
+      var previousActionTooRecent = minDifference && (now - userStorage[userStorage.length - 1] < minDifference);
+      userStorage.push(now);
+      timeouts[id] = setTimeout(function() {
+        delete storage[id];
+      }, interval);
 
       return cb(null, !tooManyActionsInInterval && !previousActionTooRecent);
 
-    });
-
+    }
   }
+
 
 };
 
 module.exports = RateLimiter;
+
+
+
