83cec73a7dbf86a2087ebed03cb27e58d2c7be74
pbwilmot <pmot199@gmail.com>
2015-11-04 07:11:41 -0800

    fixing storeblocked issue where it was skipping storing certain records

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function RateLimiter (options) {
       var batch = redis.multi();
       batch.zremrangebyscore(key, 0, clearBefore);
       batch.zrange(key, 0, -1);
-      if(!storeBlocked) {
+      if(storeBlocked) {
             batch.zadd(key, now, now);
             batch.expire(key, Math.ceil(interval/1000000)); // convert to seconds, as used by redis ttl.
       }
@@ -66,15 +66,15 @@ function RateLimiter (options) {
         if (tooManyInInterval || timeSinceLastRequest < minDifference) {
           result = Math.min(userSet[0] - now + interval, minDifference ? minDifference - timeSinceLastRequest : Infinity);
           result = Math.floor(result/1000); // convert to miliseconds for user readability.
-          if(storeBlocked) {
+        } else {
+          result = 0;
+          if(!storeBlocked) {
             batch.zadd(key, now, now);
             batch.expire(key, Math.ceil(interval/1000000)); // convert to seconds, as used by redis ttl.
             batch.exec(function (err, resultArr) {
               if (err) return cb(err);
-            }
+            });
           }
-        } else {
-          result = 0;
         }
 
         return cb(null, result)
